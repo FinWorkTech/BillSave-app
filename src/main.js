7e307cb2cbe38a60cc21fa9231a2b6c5ad64368d
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-01-27 16:25:45 -0500

    feat: set up router for the app.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createApp } from 'vue'
+
+import router from './shared/router/index.js'
 
 const app = createApp(App);
+
+// Register the router
+app.use(router);
+
 app.mount('#app');
