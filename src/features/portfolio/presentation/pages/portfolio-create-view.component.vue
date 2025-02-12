8742226f31d61feee91302fc4fe8a874762fcb2a
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-11 23:50:58 -0500

    feat(portfolio): add portfolio-create-view component.

---
+++ src/features/portfolio/presentation/pages/portfolio-create-view.component.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PortfolioForm from '../components/portfolio-form.component.vue';
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-[93vh] bg-gray-100 p-4">
+    <portfolio-form/>
+  </div>
+</template>
