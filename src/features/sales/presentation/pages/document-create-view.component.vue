4b6cce79522b0762395a58ea62236f74d5f8c09c
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-15 21:26:37 -0500

    feat(sales): add document-create-view component.

---
+++ src/features/sales/presentation/pages/document-create-view.component.vue
@@ -0,0 +1,9 @@
+<script setup>
+import DocumentForm from '../components/document-form.component.vue';
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-[93vh] bg-gray-100 p-4">
+    <document-form/>
+  </div>
+</template>
