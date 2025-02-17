bd2f17b08ca793b8098341bc4544d2205bc1531c
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-17 00:08:00 -0500

    feat(shared): add authorization interceptor for authenticated requests.

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -16,4 +16,15 @@ const http = axios.create({
 http.defaults.headers.common["Content-Type"]                = "application/json";
 http.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
+// Interceptor to add the Authorization header if the token exists
+http.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
 export default http;
