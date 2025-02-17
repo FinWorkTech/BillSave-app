8d70fa780f64f04089b6d6027499b2880403430f
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-17 00:08:41 -0500

    feat: integrate pinia for state management.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './style.css'
 import App from './App.vue'
-import { createApp } from 'vue'
 
 import router from './shared/router/index.js'
 
 const app = createApp(App);
+const pinia = createPinia();
 
+// Register the store
+app.use(pinia);
 // Register the router
 app.use(router);
 
