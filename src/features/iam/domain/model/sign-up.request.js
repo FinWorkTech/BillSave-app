3b784da1b9b70be0a02a64963ab44f6810a04301
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-17 00:03:03 -0500

    refactor(iam): add fullname field to data model.

--- src/features/iam/domain/model/sign-up.request.js
+++ src/features/iam/domain/model/sign-up.request.js
@@ -7,11 +7,13 @@ export class SignUpRequest {
   
   /**
    * Constructor
+   * @param {string} fullname - The full name
    * @param {string} username - The username
    * @param {string} password - The password
    */
-  constructor(username, password) {
-      this.username = username;
-      this.password = password
+  constructor(fullname, username, password) {
+    this.fullname = fullname;
+    this.username = username;
+    this.password = password
   }
 }
