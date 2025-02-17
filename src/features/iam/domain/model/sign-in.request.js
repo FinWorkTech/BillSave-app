5763a41d1ce052e4aeb47065eec2cad02e0e93fe
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-16 21:05:35 -0500

    feat(iam): add sign-up, sign-in response and request entities.

---
+++ src/features/iam/domain/model/sign-in.request.js
@@ -0,0 +1,17 @@
+/**
+ * SignInRequest model
+ * @summary
+ * Represents a sign-in request
+ */
+export class SignInRequest {
+  
+  /**
+   * Constructor
+   * @param {string} username - The username
+   * @param {string} password - The password
+   */
+  constructor(username, password) {
+      this.username = username;
+      this.password = password
+  }
+}
---
+++ src/features/iam/domain/model/sign-up.request.js
@@ -0,0 +1,17 @@
+/**
+ * Sign up request
+ * @summary
+ * Represents a sign-up request
+ */
+export class SignUpRequest {
+  
+  /**
+   * Constructor
+   * @param {string} username - The username
+   * @param {string} password - The password
+   */
+  constructor(username, password) {
+      this.username = username;
+      this.password = password
+  }
+}
