14ea12b62f5e4c06a30c107b5523e7bd454bcbc3
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-26 19:03:13 -0500

    refactor: overlay the toolbar view on any screen.

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import Toolbar from './public/components/toolbar.component.vue';
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <header v-if="$route.meta.showToolbar">
+    <header v-if="$route.meta.showToolbar" class="z-10">
       <Toolbar/>
     </header>
-    <main :class="['flex-grow overflow-auto', { 'lg:mt-15': $route.meta.showToolbar }]">
+    <main :class="['flex-grow overflow-auto', { 'lg:mt-15': $route.meta.showToolbar }]" class="z-0">
       <router-view/>
     </main>
   </div>
