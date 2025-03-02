04125c383a09b95d20ed8cf0ee36f7fc202e0278
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-03-01 22:57:04 -0500

    feat(shared): add local-storage caching for exchange rate data in sunat store.

--- src/shared/presentation/store/sunat.store.js
+++ src/shared/presentation/store/sunat.store.js
@@ -1,6 +1,8 @@
 import { defineStore } from "pinia";
 import SunatService from '@shared/services/sunat.service.js';
 
+const LS_SUNAT_KEY = "sunat_exchange_rate";
+
 export const useSunatStore = defineStore("sunat", {
 
   state: () => ({
@@ -11,8 +13,12 @@ export const useSunatStore = defineStore("sunat", {
   actions: {
     async fetchExchangeRate(date) {
 
-      if (this.date == date) {
+      const cachedData = JSON.parse(localStorage.getItem(LS_SUNAT_KEY));
+
+      if (cachedData && cachedData.date === date) {
         console.log('Using cached exchange rate for date', date);
+        this.salePrice = cachedData.salePrice;
+        this.date = cachedData.date;
         return;
       }
 
@@ -20,10 +26,24 @@ export const useSunatStore = defineStore("sunat", {
         const exchangeRate = await SunatService.fetchExchangeRate(date);
         this.salePrice = exchangeRate.precioVenta;
         this.date = exchangeRate.fecha;
+
+        localStorage.setItem(LS_SUNAT_KEY, JSON.stringify({
+          salePrice: exchangeRate.precioVenta,
+          date: exchangeRate.fecha
+        }));
       }
       catch (err) {
         console.error('Error fetching exchange rate: ', err);
         throw err;
+      }
+    },
+
+    loadExchangeRateFromStorage() {
+      const cachedData = JSON.parse(localStorage.getItem(LS_SUNAT_KEY));
+      
+      if (cachedData) {
+        this.salePrice = cachedData.salePrice;
+        this.date = cachedData.date;
       }
     }
   }
